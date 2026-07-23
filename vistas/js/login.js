aa6056f38e76672ff1a6550d0c89ffbd2a9bb762
Alberto Ferreira <alberto.ferreira@somosetica.com>
2026-07-23 14:48:45 -0300

    finalizado modulo 2

--- vistas/js/login.js
+++ vistas/js/login.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
             });
             return;
         }
-
+ 
         $.ajax({
             url: "../api-rma/sesiones/funLogin.php",
             type: "POST",
